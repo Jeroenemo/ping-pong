bc4ab9bc041f9292d53c5731a9a9aefd4873895f
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-22 16:19:08 -0500

    pass all four specs for ping-pong
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>
    Co-authored-by: Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>

--- Program.cs
+++ Program.cs
@@ -8,6 +8,26 @@ class Program
     
     
     Console.WriteLine("Please provide an integer greater than 1:");
-    IndexVariableCaptured = parse.Int(Console.ReadLine());
+    int inputNumber = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= inputNumber; i++)
+    {
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine("Ping-Pong");
+      }
+      else if (i % 3 == 0)
+      {
+        Console.WriteLine("Ping");
+      }
+      else if (i % 5 == 0)
+      {
+        Console.WriteLine("Pong");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
