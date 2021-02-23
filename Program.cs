1cfd39b72b2baab5652bcb96cfc65992a9a7a425
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-22 20:32:21 -0500

    remove comments, manually lint code

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ class Program
 {
   static void Main()
   {
-    // program code goes here
-    
-    
     Console.WriteLine("Please provide an integer greater than 1:");
     int inputNumber = int.Parse(Console.ReadLine());
 
